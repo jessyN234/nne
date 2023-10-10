abcf3a73b086382fc78d31367837a754175e0716
jessyN234 <jessicankwocha71@gmail.com>
2023-10-10 15:11:39 +0100

    first commit

---
+++ excel.js
@@ -0,0 +1,29 @@
+//  var fristNumber = prompt("enter first number");
+//  var secondNumber = prompt(" enter second number");
+//  var thirdNumber = prompt(" enter third number");
+//  var fourthNumber = prompt(" enter fourth number");
+//  var fifthNumber = prompt(" enter fifth number");
+//  var sixthNumber = prompt(" enter sixth number");
+//  var seventhNumber = prompt(" enter seventh number");
+//  var eightNumber = prompt(" enter eightnumber");
+// //  var age = prompt("What is your age");
+
+// alert(Number(fristNumber) + Number(secondNumber) + Number(thirdNumber) + Number(fourthNumber) + Number(fifthNumber) + Number(sixthNumber) + Number(seventhNumber) + Number(eightNumber));
+// console.log("your full name is " + fullName );
+// console.log("you are " + age + " years old");
+
+// var age = prompt("What is your age");
+// var days = age * 365.25
+//  alert("you are roughly " + days + " years old")
+
+var age = prompt("what is your age");
+if(age > 18) {
+    alert("sorry you can not enter");
+}
+
+else if(age > 21) {
+    alert("you can enter bt dont drink");
+}
+else {
+    alert("you can drink")
+}
